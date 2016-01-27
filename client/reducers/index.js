c86f7ebd7f82b0eba857ece3b4466ef85f37fe40
Ian Liu <ianliu@Ians-MacBook-Pro-2.local>
2016-01-27 15:50:38 -0800

    Added react-router-redux

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,9 +1,11 @@
 import merge from 'lodash/object/merge'
-import { routerStateReducer as router } from 'redux-router'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistory, routeReducer } from 'react-router-redux'
 import { combineReducers } from 'redux'
 import matchmaker from './matchmaker'
 
 const rootReducer = combineReducers({
+  routing: routeReducer,
   matchmaker
 })
 
