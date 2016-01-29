ff704a64b16cf7040060fc12a70f0c72698ee647
JC <jc@jcmbp.local>
2016-01-29 15:15:32 -0800

    now returns connected pairs and messages on api/chats

--- db/chatHelpers.js
+++ db/chatHelpers.js
@@ -8,3 +8,19 @@ export function getMessagesForUser(user_id) {
 	inner join users on users.user_id=pairs.user_one or users.user_id=pairs.user_two\n\
 	where users.user_id=${user_id};`)
 }
+
+export function getConnectedPairsAndMessagesForUser(user_id) {
+	return db.query(
+		`select pairs.*, messages.*\n\
+		from messages\n\
+		full outer join pairs on messages.pair_id=pairs.pair_id\n\
+		inner join users on users.user_id=pairs.user_one or users.user_id=pairs.user_two\n\
+		where users.user_id=${user_id} and pairs.connected=false;` //for testing purposes, pairs.connected=false. Set it to true for production.
+	)
+}
+
+export function addMessage(msgObj) {
+	// return db.query(
+	// 	`insert into messages (pair_id, sender, text, created_at) values(${msgObj}`
+	// )
+}
