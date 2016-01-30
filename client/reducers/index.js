154165c939d2ba0e08b7764b5dad0fd7ee1cfacb
JC <jc@jcmbp.local>
2016-01-30 12:01:22 -0800

    deleted reference to unused auth reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,7 +3,6 @@ import { combineReducers } from 'redux';
 import matchmaker from './matchmaker';
 import scoreboard from './scoreboard';
 import user from './user';
-import authentication from './authentication';
 import {reducer as formReducer} from 'redux-form';
 
 const rootReducer = combineReducers({
