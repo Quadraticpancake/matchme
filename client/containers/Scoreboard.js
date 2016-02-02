b1c8bdabfd98b8c910d00593ede38e103137a65e
Ian Liu <ianliu@Ians-MacBook-Pro-2.local>
2016-02-01 18:54:37 -0800

    Undo div height change

--- client/containers/Scoreboard.js
+++ client/containers/Scoreboard.js
@@ -4,16 +4,15 @@ import { connect } from 'react-redux';
 import * as ScoreboardActions from '../actions/scoreboard';
 import { socket } from './App'
 
-const height = $('body').height();
 const scoreboardStyle = {
   position: 'absolute',
   width: 230,
   right: 0,
   top: 0,
   marginTop: 50,
+  bottom: 0,
   backgroundColor: '#eee',
-  height: height,
-  // borderRight: '1px solid #333',
+  borderRight: '1px solid #333',
   // padding: 16
 }
 
