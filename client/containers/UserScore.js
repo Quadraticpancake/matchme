c6e070e20a80e71035172cc3153e9986b3764d8b
staybailey <staybailey@gmail.com>
2016-02-02 17:01:22 -0800

    Added some styling and refactored scoring to ensure that score gets displayed even if you haven't created a connection

--- client/containers/UserScore.js
+++ client/containers/UserScore.js
@@ -10,6 +10,35 @@ import Matchee from '../components/Matchee';
 //   dispatch => bindActionCreators(actionCreators, dispatch)
 // )
 
+const divStyle = {
+  // width: 400,
+//  height: 40,
+  width: 'auto',
+  // marginTop: 40,
+//  borderWidth: 1,
+  borderColor: 'black',
+  // opacity: .5,
+  // backgroundColor: '#ccc',
+
+  // display: 'block',
+  // position:'relative',
+  // verticalAlign: 'bottom',
+
+  // backgroundImage: 'url(' + image_url + ')',
+  // backgroundSize: 'cover',
+
+  fontSize: 30,
+  fontWeight: 'bold',
+  fontFamily: 'Helvetica, sans-serif',
+  // backgroundImage: 'http://i.onionstatic.com/onion/7954/original/1200.jpg',
+  // WebkitTextFillColor: 'white',  Will override color (regardless of order)
+  // WebkitTextStrokeWidth: 2,
+  // WebkitTextStrokeColor: 'black',
+
+  //borderRadius: 5,
+  //zIndex: 1
+};
+
 class UserScore extends Component {
   // constructor(props) {
   //   super(props);
@@ -26,22 +55,26 @@ class UserScore extends Component {
     // for testing purposes
     
     let renderedConnectionsMade = [];
-    
-    for (var i = 0; i < (userScore ? userScore.length : 0); i++) {
-      renderedConnectionsMade.push(<div className="container">
-                                     <div className="row-fluid">
-                                       <div className='col-md-4'>
-                                         <Matchee matchee={userScore[i].user_one} />
-                                         <Matchee matchee={userScore[i].user_two} />
+    let score = 0;
+    if (userScore) {
+      console.log(userScore);
+      score = userScore.score;
+      for (var i = 0; i < (userScore.pairs.length : 0); i++) {
+        renderedConnectionsMade.push(<div className="container">
+                                       <div className="row-fluid">
+                                         <div className="col-md-4">
+                                           <Matchee matchee={userScore.pairs[i].user_one} />
+                                           <Matchee matchee={userScore.pairs[i].user_two} />
+                                         </div>
                                        </div>
-                                     </div>
-                                   </div>);
+                                     </div>);
+      }
     }
     
 
     return (
       <section>
-        {<div><div>These are the wonderful matches you have made</div>{renderedConnectionsMade}</div>}
+        {<div><div className='well well-sm col-md-6 col-sm-12 col-xs-12' style={divStyle}>Your score is { score } and these are the wonderful connections you helped create</div>{renderedConnectionsMade}</div>}
       </section>
     );
   }
