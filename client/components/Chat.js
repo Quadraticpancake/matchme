c1920a3334d2c2eae649244e580b5c436ea4e49a
JC <jc@jcmbp.local>
2016-02-04 20:07:53 -0800

    added heart svg element

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
-import { Message } from './Message'
+import { Message } from './Message';
+import heart from '../../static/img/icons/heart'; // Heart is an SVG graphic from: http://codepen.io/sol0mka/pen/c6721c06e93b4ee6cc95a21f6a4caedc
+import { extend } from 'underscore';
+
+const heartSvg = heart();
+const heartNotFilledInStyle = {
+  width: '32px',
+  height: '32px',
+  fill: '#ccc'
+};
+const heartFilledInStyle = extend({}, heartNotFilledInStyle, {fill: '#FE4365'});
 
 const chatStyle = {
   padding: '10px'
@@ -23,18 +33,26 @@ export class Chat extends Component {
     let renderedMessages = [];
     const heartButton = 'https://freeiconshop.com/files/edd/heart-compact-flat.png';
 
-
+    let is_user_one;
     if (chat) {
-      const is_user_one = chat.user_one.user_id === user_id;
+      is_user_one = chat.user_one.user_id === user_id;
       chat.messages.forEach((message, index) => {
         renderedMessages.push(<Message key={index} message={message} chat={chat} sender={message.sender === chat.user_one.user_id ? chat.user_one : chat.user_two}/>);
       });
-
+    }
       return (
         <div className='col-md-10 col-sm-11 col-xs-11 well' style={chatStyle}>
+          {/* SVG Heart: heartSVG contains the definition of a SVG heart*/}
+          { heartSvg }
+          {/* SVG Heart: the below SVG element actually places the SVG heart on the page*/}
+          <svg viewBox="0 0 32 32" style={chat && chat.userHeart === true ? heartFilledInStyle : heartNotFilledInStyle}>
+             <g filter="url(#inset-shadow)">
+              <use xlinkHref="#heart-icon"></use>
+            </g>
+          </svg>
           <div style={{'display': 'flex'}}>
             <img src={heartButton} style={heartStyle} onClick={() => heartConnection(pair_id, user_id, is_user_one)} />
-            Like this match? Let us know! The other user won't know you gave them a heart unless you gave them one too.
+            Like this match? Let us know! The other user won't know you gave them a heart unless they give you one too.
           </div>
           <div>
             {renderedMessages.length > 0 ? renderedMessages : "No messages yet"}
@@ -47,9 +65,9 @@ export class Chat extends Component {
         </div>
       );
       
-    } else {
-      return (<div />)
-    }
+    // } else {
+    //   return (<div />)
+    // }
   }
 }
 
