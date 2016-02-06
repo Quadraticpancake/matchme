82613e22b483b53d57b44f1a526800bbe04731ac
JC <jc@jcmbp.local>
2016-02-06 15:38:53 -0800

    styled ProfileForm

--- server/routes.js
+++ server/routes.js
@@ -60,6 +60,7 @@ export default function (app, express) {
 	//api/users:user_id/chats
 	app.get('/api/chats/:user_id', (req, res) => {
 		getConnectedPairsAndMessagesForUser(req.params.user_id).then((rows) => {
+			console.log('chats', rows)
 			res.json(rows)
 		});
 	});
