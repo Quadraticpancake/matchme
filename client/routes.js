ab7bca8898c28310c461118c083c6c8a81158561
Rachel RoseFigura <rachel.rosefigura@gmail.com>
2016-02-08 12:32:01 -0800

    create front end route for recommendations

--- client/routes.js
+++ client/routes.js
@@ -6,8 +6,9 @@ import App from './containers/App';
 import Home from './containers/Home';
 import Profile from './containers/Profile.js';
 import Chats from './containers/Chats.js';
-import Score from './containers/UserScore.js'
-import ProfilePicture from './containers/ProfilePicture.js'
+import Score from './containers/UserScore.js';
+import ProfilePicture from './containers/ProfilePicture.js';
+import Recommendation from './containers/Recommendation.js';
 
 export default (
   <Route path="/" component={App}>
@@ -16,6 +17,7 @@ export default (
     <Route path="chats" component={Chats} />
     <Route path="score" component={Score} />
     <Route path="pictures" component={ProfilePicture}/>
+    <Route path="recommendation" component={Recommendation}/>
     <Route status={404} path="*" component={Home} />
   </Route>
 );
