e9b3a80fb4d8581eefc29784774ad6130ec22772
Rachel RoseFigura <rachel.rosefigura@gmail.com>
2016-02-08 12:31:28 -0800

    create front end route for recommendations

--- server/faceAnalysis/faceAnalysis.js
+++ server/faceAnalysis/faceAnalysis.js
@@ -1,33 +1,50 @@
 var request = require('request');
-var url = 'https://upload.wikimedia.org/wikipedia/commons/d/d2/Michelle-Borromeo-Actor-Headshots-Vancouver-BC20110408_0030.jpg';
+var url = 'http://i.imgur.com/76tI0oc.jpg';
+import { faceAnalytics } from '../db/dbHelpers';
+var user_id = 1;
 
-var options = {
-  url: 'https://faceplusplus-faceplusplus.p.mashape.com/detection/detect?url=' + url,
-  headers: {
-    "X-Mashape-Key": "IJ3moiqJsymshUNl41Sq09a38nMjp1OIrd9jsnJw6JjvebyGo5",
-    "Accept": "application/json"
-  }
+function analyzeFace() {
+	var options = {
+	  url: 'https://faceplusplus-faceplusplus.p.mashape.com/detection/detect?url=' + url,
+	  headers: {
+	    "X-Mashape-Key": "IJ3moiqJsymshUNl41Sq09a38nMjp1OIrd9jsnJw6JjvebyGo5",
+	    "Accept": "application/json"
+	  }
+	};
+
+	function callback(error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    var info = JSON.parse(body);
+	    var faceAnalytics = {
+	    	age: null,
+	    	coloring: null,
+	    	expression: null,
+	    	faceShape: null
+	    };
+
+	    if (info.face[0]) {
+		    faceAnalytics = {
+		    	age: info.face[0].attribute.age.value,
+		    	coloring: info.face[0].attribute.race.value,
+		    	expression: info.face[0].attribute.smiling.value,
+		    	faceShape: info.face[0].position.width / info.face[0].position.height
+		    };	
+	    }; 
+
+	    console.log('info from face analysis request', faceAnalytics);
+	    putAnalytics(user_id, faceAnalytics);
+
+	  }
+	}
+
+	request(options, callback);
 };
 
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    console.log('info from face analysis request', info.face[0]);
-  }
-}
 
-request(options, callback);
+// analyzeFace();
 
 // analytics
-// attribute.age.value
+// 
 // attribute.race.value
 // attribute.smiling.value
-// position.height
-// position.width
-
-// compare:
-// | age diff |
-// | smiling diff |
-// | height/width diff |
-// race same = 0, not same = some number
-
+// position.height / position.width
