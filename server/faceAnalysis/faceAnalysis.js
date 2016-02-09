a17707067cecb6de6700becd191617bcf5183825
Rachel RoseFigura <rachel.rosefigura@gmail.com>
2016-02-09 08:52:39 -0800

    url encoding facebook pictures for getting image analytics

--- server/faceAnalysis/faceAnalysis.js
+++ server/faceAnalysis/faceAnalysis.js
@@ -1,10 +1,14 @@
+'use strict';
+
+
 var request = require('request');
 
-export default function generateUserAnalytics() {
-	let url = 'http://i.imgur.com/CQ8vAZP.jpg';
+export default function generateUserAnalytics(id, url) {
+
+	let encodedURI = encodeURIComponent(url);
 
 	var options = {
-	  url: 'https://faceplusplus-faceplusplus.p.mashape.com/detection/detect?url=' + url,
+	  url: 'https://faceplusplus-faceplusplus.p.mashape.com/detection/detect?url=' + encodedURI,
 	  headers: {
 	    "X-Mashape-Key": "IJ3moiqJsymshUNl41Sq09a38nMjp1OIrd9jsnJw6JjvebyGo5",
 	    "Accept": "application/json"
@@ -13,8 +17,11 @@ export default function generateUserAnalytics() {
 
 	function callback(error, response, body) {
 	  if (!error && response.statusCode == 200) {
+	  	console.log(body);
+	  	console.log(error);
 	    var info = JSON.parse(body);
 	    var faceAnalytics = {
+	    	user_id: null,
 	    	age: null,
 	    	coloring: null,
 	    	expression: null,
@@ -23,6 +30,7 @@ export default function generateUserAnalytics() {
 
 	    if (info.face[0]) {
 		    faceAnalytics = {
+		    	user_id: id,
 		    	age: info.face[0].attribute.age.value,
 		    	coloring: info.face[0].attribute.race.value,
 		    	expression: info.face[0].attribute.smiling.value,
@@ -39,6 +47,6 @@ export default function generateUserAnalytics() {
 	return request(options, callback);
 };
 
-// generateUser(1, 'http://i.imgur.com/CQ8vAZP.jpg')
+// generateUserAnalytics(1, "https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xfa1/v/t1.0-9/12376423_10205626501086765_5111400853167845939_n.jpg?oh=b653caef995bb365c0bba94a54612d85&oe=576ADD93");
 
 
