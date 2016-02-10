e4dd77ed15e9517a041864fc14d703f243abbac8
Ian Liu <ianliu@Ians-MacBook-Pro-2.local>
2016-02-10 09:14:25 -0800

    Got rid of rerouting if unauthenticated

--- client/containers/Chats.js
+++ client/containers/Chats.js
@@ -44,12 +44,12 @@ class Chats extends Component {
   }
 
   componentWillMount(){
-    const { user, routerActions } = this.props;
-    //if user isn't authenticated reroute them to the home page
-    if (!user.isAuthenticated) {
-      routerActions.push('/home');
-      return;
-    }
+    // const { user, routerActions } = this.props;
+    // //if user isn't authenticated reroute them to the home page
+    // if (!user.isAuthenticated) {
+    //   routerActions.push('/home');
+    //   return;
+    // }
   }
 
   componentDidMount() {
@@ -98,13 +98,13 @@ class Chats extends Component {
       );
     } else {
       return (
-        <Chat 
+        <Chat
           key={focus}
-          chat={focusedChat} 
-          addMessageOnEnter={this.addMessageOnEnter.bind(this)} 
-          pair_id={focus} 
-          user_id={user_id} 
-          heartConnection={actions.heartConnection} 
+          chat={focusedChat}
+          addMessageOnEnter={this.addMessageOnEnter.bind(this)}
+          pair_id={focus}
+          user_id={user_id}
+          heartConnection={actions.heartConnection}
           closeChat={actions.closeChat}
           collapseChat={actions.collapseChat}
         />
