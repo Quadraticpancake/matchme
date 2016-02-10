9d7375ee6e0248f7d0da12147108fa14fdf05b28
Ian Liu <ianliu@Ians-MacBook-Pro-2.local>
2016-02-09 22:33:02 -0800

    Changed database_url

--- db/config.js
+++ db/config.js
@@ -1,14 +1,15 @@
 //create connection here
 var bluebird = require('bluebird');
 var pgp = require('pg-promise')({promiseLib: bluebird});
-var connectionString = process.env.DATABASE_URL||"postgres://postgres:test@localhost:5432/matchmaker";
+// var connectionString = process.env.DATABASE_URL||"postgres://postgres:test@localhost:5432/matchmaker";
+var connectionString = process.env.DATABASE_URL;
 
 //create new db instance
 var db = pgp(connectionString);
 module.exports = db;
 
 // install postgres: https://www.moncefbelyamani.com/how-to-install-postgresql-on-a-mac-with-homebrew-and-lunchy/
-// to test locally you must create a database 'matchmaker' in postgres 
+// to test locally you must create a database 'matchmaker' in postgres
 // and set up your postgres user to have password 'test'
     // alter role postgres with password 'test';
 	// CREATE DATABASE matchmaker;
