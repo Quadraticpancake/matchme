31dcba271cc1783af2178f657a0bb755051fa025
Ian Liu <ianliu@Ians-MacBook-Pro-2.local>
2016-02-12 10:02:07 -0800

    Changed facebook app id

--- client/containers/App.js
+++ client/containers/App.js
@@ -43,32 +43,23 @@ class App extends Component {
           }
         }
 
-        window.fbAsyncInit = function() {
-          FB.init({
-            appId      : '1640124032916803',
-            cookie     : true,  // enable cookies to allow the server to access
-                                // the session
-            xfbml      : true,  // parse social plugins on this page
-            version    : 'v2.2' // use version 2.2
-          });
-
-          FB.getLoginStatus(function(response) {
-            statusChangeCallback(response);
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '991657460895356',
+          xfbml      : true,
+          version    : 'v2.5'
         });
+      };
 
-        };
+      (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
 
-        // Load the SDK asynchronously
-
-        (function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = "//connect.facebook.net/en_US/sdk.js";
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-
-  }
+      };
 
   render() {
     window.AppProps = this.props;
