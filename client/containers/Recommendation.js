80478942319eafafab90dd07bb8d6e84048dfc74
Ian Liu <ianliu@Ians-MacBook-Pro-2.local>
2016-02-11 22:19:06 -0800

    Some styling updates.

--- client/containers/Recommendation.js
+++ client/containers/Recommendation.js
@@ -18,7 +18,7 @@ class Recommendation extends Component {
 
     // <img src='http://i.imgur.com/nTpf2tW.gif'/>
     const {recommendation, user, actions, buyActions} = this.props;
-    
+
     // gender icon
     let maleIcon = 'http://1.bp.blogspot.com/-9zJZ2kiHqFQ/VQCayOG1pxI/AAAAAAAADEU/igsvbvsPjKU/s1600/The%2BMale%2BPrinciple.png';
     let femaleIcon = 'http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/3d-transparent-glass-icons-symbols-shapes/016921-3d-transparent-glass-icon-symbols-shapes-female-symbol.png';
@@ -43,11 +43,11 @@ class Recommendation extends Component {
 
     return (
 
-      <div style={{marginLeft: '2%', marginTop: '2%'}}>
+      <div style={{marginLeft: '2%', marginTop: '0%'}}>
         <div className={css.algorithmDescription}>
-          <h2>Let our algorithm help you find the perfect match...</h2>
-          <h4>Looks are important. Our algorithm analyzes the matches that you've 'hearted' and conducts facial image analysis on their pictures to find other users you may like the look of. Try it now!</h4>
-          <h6>Our robot can't get you matches until you've hearted matches. Go to your chats and heart your favorite matches!</h6>
+          <h2 className={css.header2}>Let our algorithm help you find the perfect match...</h2>
+          <h4 className={css.header4}>Looks are important. Our algorithm analyzes the matches that you've 'hearted' and conducts facial image analysis on their pictures to find other users you may like the look of. Try it now!</h4>
+          <h6 className={css.header6}>Our robot can't get you matches until you've hearted matches. Go to your chats and heart your favorite matches!</h6>
         </div>
         <div className={css.robotDivStyle}>
           <img className={css.robotsStyle} src='http://i.imgur.com/83p3mf2.gif'/>
@@ -58,11 +58,11 @@ class Recommendation extends Component {
           <div className={css.prospectInfoStyle}>
             <h4 className={css.nameStyle}> {recommendation.first_name}, {age} <img src={icon_path} className={css.iconProspectStyle}/> </h4>
             <div className={css.prospectInfo}>''{recommendation.description}''</div>
-            
+
             <a href="/chats">
             <BuyRecommendation dis={user.userScore.score < 1000 || recommendation.gender === 'none'} actions={buyActions} person={recommendation} user={user}/>
             </a>
-            
+
           </div>
 
         </div>
