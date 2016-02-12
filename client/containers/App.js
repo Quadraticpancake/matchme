246c57b3b7fcc57fa9af21e55f122f931c978d4e
Ian Liu <ianliu@Ians-MacBook-Pro-2.local>
2016-02-12 10:45:54 -0800

    Trying to fix fb login on init.

--- client/containers/App.js
+++ client/containers/App.js
@@ -20,12 +20,16 @@ class App extends Component {
     componentWillMount(){
       const { actions, routeActions } = this.props;
         function statusChangeCallback(response) {
+          console.log('statusChangeCallback!');
+          console.log(response);
+
           // The response object is returned with a status field that lets the
           // app know the current login status of the person.
           // Full docs on the response object can be found in the documentation
           // for FB.getLoginStatus().
           if (response.status === 'connected') {
             // Logged into your app and Facebook.
+            console.log('connected!');
             let userID = response.authResponse.userID;
             let accessToken = response.authResponse.accessToken;
             actions.login(userID, accessToken);
@@ -43,26 +47,30 @@ class App extends Component {
           }
         }
 
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '991657460895356',
-          xfbml      : true,
-          version    : 'v2.5'
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId      : '991657460895356',
+            cookie     : true,  // enable cookies to allow the server to access
+                                // the session
+            xfbml      : true,  // parse social plugins on this page
+            version    : 'v2.2' // use version 2.2
+          });
+
+          FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
         });
 
-        FB.getLoginStatus(function(response) {
-          statusChangeCallback(response);
-        });
-      };
+        };
 
-      (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
+        // Load the SDK asynchronously
 
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/sdk.js";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
       };
 
   render() {
